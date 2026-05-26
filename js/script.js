8a552dad5f3b74d6ae227db6925b6e311e041626
Carlos-Torres-Class <carlos.torres138@upr.edu>
2026-05-25 22:29:38 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,3 +13,40 @@ function validarFormulario() {
     alert("Formulario enviado correctamente.");
     return true;
 }
+let slideIndex = 1;
+
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let captionText = document.getElementById("caption");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    captionText.innerHTML = slides[slideIndex - 1].querySelector("img").alt;
+}
